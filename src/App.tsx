76dcce6a00da20e203c21e75f1981f8a9dbaef42
ashishkunthe <ashishkunthe2003@gmail.com>
2025-03-08 18:47:29 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./Components/Button";
+
+function App() {
+  return (
+    <>
+      <Button
+        text="Click Here"
+        variant="secondary"
+        size="lg"
+        onClick={() => {}}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Button.tsx
@@ -0,0 +1,34 @@
+export interface ButtonProps {
+  variant: "primary" | "secondary";
+  size: "sm" | "md" | "lg";
+  text: string;
+  startIcon?: any;
+  endIcon?: any;
+  onClick: () => void;
+}
+
+export const Button = ({
+  variant,
+  size,
+  text,
+  startIcon,
+  endIcon,
+  onClick,
+}: ButtonProps) => {
+  const baseStyles = "rounded px-4 py-2 font-medium flex items-center gap-2";
+  const variantStyles =
+    variant === "primary" ? "bg-blue-600 text-white" : "bg-gray-300 text-black";
+  const sizeStyles =
+    size === "sm" ? "text-sm" : size === "md" ? "text-base" : "text-lg";
+
+  return (
+    <button
+      onClick={onClick}
+      className={`${baseStyles} ${variantStyles} ${sizeStyles}`}
+    >
+      {startIcon && <span>{startIcon}</span>}
+      {text}
+      {endIcon && <span>{endIcon}</span>}
+    </button>
+  );
+};
